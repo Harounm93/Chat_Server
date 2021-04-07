9459006725e6031b2834dcd3d748731f7ae81d47
harounm93 <mahdiharoun93@gmail.com>
2021-04-07 19:43:26 +0100

    cors issue progress

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const cors = require('cors')
 
-io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
-  })
-})
+// io.on('connection', socket => {
+//   socket.on('message', ({ name, message }) => {
+//     io.emit('message', { name, message })
+//   })
+// })
 
-http.listen(4000, function() {
-  console.log('listening on port 4000')
-})
+// http.listen(4000, function() {
+//   console.log('listening on port 4000')
+// })
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "tp://localhost:4000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
